ea1280ef6b95bc217cf414c846899103944a4acb
Serdar Mustafa <serdar@bolt.ee>
2020-06-19 10:26:30 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+// Middlewares
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+// app.use((req, res, next) => {
+//   console.log('hello from middleware 🚀');
+//   next();
+// });
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  // console.log(req.headers);
+  next();
+});
+
+// Routes
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Cant't find ${req.originalUrl} on this server`));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
